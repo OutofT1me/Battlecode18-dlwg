895104d5c0d74e12546f4b5e0fefa73d71e2a016
Oliver Leung <oliverleung2@gmail.com>
2018-01-19 09:04:13 -0600

    Add upgrade queue, cleaned style

--- src/Player.java
+++ src/Player.java
@@ -1,31 +1,21 @@
+package src;
+
 // import the API.
 // See https://s3.amazonaws.com/battlecode-2018/api/java/bc/package-summary.html for the javadocs.
-import java.util.LinkedList;
-import java.util.List;
-
+import java.util.*;
 import bc.*;
 
 public class Player {
-	
 
     public static void main(String[] args) {
-    	
-        // MapLocation is a data structure you'll use a lot.
-        MapLocation loc = new MapLocation(Planet.Earth, 10, 20);
-        System.out.println("loc: "+loc+", one step to the Northwest: "+loc.add(Direction.Northwest));
-        System.out.println("loc x: "+loc.getX());
 
-        // One slightly weird thing: some methods are currently static methods on a static class called bc.
-        // This will eventually be fixed :/
-        System.out.println("Opposite of " + Direction.North + ": " + bc.bcDirectionOpposite(Direction.North));
-
-        // Connect to the manager, starting the game
+        // Starts game and initializes processes
         GameController gc = new GameController();
-       
-     
+        queueUpgrades(gc);
+
         while (true) {
             System.out.println("Current round: "+gc.round());
-            // VecUnit is a class that you can think of as similar to ArrayList<Unit>, but immutable.
+
             VecUnit units = gc.myUnits();
             for (int i = 0; i < units.size(); i++) {
                 Unit unit = units.get(i);
@@ -35,79 +25,49 @@ public class Player {
                     gc.moveRobot(unit.id(), Direction.Southeast);
                 }
             }
+
             // Submit the actions we've done, and wait for our next turn.
             gc.nextTurn();
         }
     }
-    
-    // Ordered clockwise, does not include Direction.Center
-    Direction[] directions = {Direction.North, Direction.Northeast, Direction.East, Direction.Southeast, Direction.South, Direction.Southwest, Direction.West, Direction.Northwest};
-    
-    public Direction RotateLeft(Direction initial) {
-   		if (initial == Direction.North) return Direction.Northwest;
-   		for (int i=1; i<=8;i++) {
-   			if (directions[i] == initial){
-   				return directions[i-=1];
-   			}
-   		} 
-        return Direction.North;
-   	}
-    
-   	public Direction RotateRight(Direction initial) {
-   		if (initial == Direction.Northwest) return Direction.North;
-   		for (int i=0; i<8;i++) {
-   			if (directions[i] == initial) {
-   				return directions[i++];
-   			}
-   		} 
-        return Direction.North;
-   	}
-    
-    public void goThere(Unit Robot, MapLocation Destination, GameController gc) {
-        while (gc.isMoveReady(Robot.id())) {
-            if (gc.canMove(Robot.id(), Robot.location().mapLocation().directionTo(Destination))) {
-                gc.moveRobot(Robot.id(), Robot.location().mapLocation().directionTo(Destination));
-            }
-        }
-    }
-   
 
-    /*
-    public LinkedList flutterPath (Unit Robot, MapLocation Destination, int maxlength, GameController gc) {
-    list<MapLocation> path = new LinkedList();
-    MapLocation temp = Robot.mapLocation();
-    path.add(temp);
-    int n = 0;
-    while (temp.equals(Destination) == false& n <= maxlength) {
-        if (gc.isOccupiable(temp.add(temp.directionTo(Destination)))==true){
-            temp = temp.add(temp.directionTo(Destination));
-            path.add(temp);
-        } else if (gc.isOccupiable(temp.add(RotateRight(temp.directionTo(Destination)))==true)) {
-            temp = temp.add(RotateRight(temp.directionTo(Destination)));
-            path.add(temp);
-        } else if (gc.isOccupiable(temp.add(RotateLeft(temp.directionTo(Destination)))==true)) {
-            temp = temp.add(RotateLeft(temp.directionTo(Destination)));
-            path.add(temp);
-        } else if (gc.isOccupiable(temp.add(RotateRight(RotateRight(temp.directionTo(Destination))))==true)) {
-            temp = temp.add(RotateRight(RotateRight(temp.directionTo(Destination))));
-            path.add(temp);
-        } else if (gc.isOccupiable(temp.add(RotateLeft(RotateLeft(temp.directionTo(Destination))))==true)) {
-            temp = temp.add(RotateLeft(RotateLeft(temp.directionTo(Destination))));
-            path.add(temp);
-        } else if (gc.isOccupiable(temp.add(RotateRight(RotateRight(temp.directionTo(Destination))))==true)) {
-            temp = temp.add(RotateRight(RotateRight(temp.directionTo(Destination))));
-            path.add(temp);
-        }else if (gc.isOccupiable(temp.add(RotateRight(RotateRight(RotateRight(temp.directionTo(Destination)))))==true)) {
-            temp = temp.add(RotateRight(RotateRight(RotateRight(temp.directionTo(Destination)))));
-            path.add(temp);
-        }else if (gc.isOccupiable(temp.add(RotateLeft(RotateLeft(RotateLeft(temp.directionTo(Destination)))))==true)) {
-            temp = temp.add(RotateRight(RotateRight(RotateR(temp.directionTo(Destination)))));
-            path.add(temp);
-        } else return null;
-        n++;
-        }  
-        
-        if (n==maxlength) return null;
+    // Queues upgrades at the start of the game
+    public static void queueUpgrades(GameController gc) {
+
+        // For most important units
+        for (int lvl = 1; lvl <= 3; lvl++) {
+            gc.queueResearch(UnitType.Worker);
+            gc.queueResearch(UnitType.Ranger);
+            gc.queueResearch(UnitType.Rocket);
+        }
+        gc.queueResearch(UnitType.Worker); // Extra Worker lvl4 upgrade
+
+        // For less important units
+        for (int lvl = 1; lvl <= 3; lvl++) {
+            gc.queueResearch(UnitType.Knight);
+            gc.queueResearch(UnitType.Mage);
+            gc.queueResearch(UnitType.Healer);
+        }
+        gc.queueResearch(UnitType.Mage); // Extra Mage lvl4 upgrade
+
+    }
+    
+    // Generates a basic path for a robot
+    // Gets blocked by obstacles, so don't use in its current state
+    public LinkedList basicPath(Unit robot, MapLocation Destination, GameController gc) {
+        LinkedList<MapLocation> path = new LinkedList();
+        MapLocation currentLoc = robot.location().mapLocation();
+        path.add(currentLoc);
+        int pathLength = 0;
+        while (!currentLoc.equals(Destination)) {
+            if (gc.isOccupiable(currentLoc.add(currentLoc.directionTo(Destination))) > 0){
+                currentLoc = currentLoc.add(currentLoc.directionTo(Destination));
+                path.add(currentLoc);
+            }
+            else return null;
+            pathLength++;
+        }
+
         return path;
-    } */
+    }
 }
